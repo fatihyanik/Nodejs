fcdb8847a265ddb01942f7ae887864f7decc724f
fatihyanik <fatihyanik07@gmail.com>
2022-01-25 09:02:46 +0100

    i added partials folder

--- 8-view-engine/app.js
+++ 8-view-engine/app.js
@@ -2,53 +2,84 @@ const express = require("express");
 const logger = require("morgan");
 
 const app = express();
-
 // setting the port
 app.set("port", process.env.PORT || 3000);
 // setting a view engine ...
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
-
+// Set public folder
+app.use('/public', express.static(__dirname + '/public'))
+// add bootstrab folder from node_modules
+app.use('/bootstrap', express.static(__dirname + "/node_modules/bootstrap"))
+app.use('/jquery', express.static(__dirname + "/node_modules/jquery"))
 app.use(logger("dev"));
 
 // getting main request
 app.get("/", (req, res) => {
-  //res.sendFile(__dirname + "/index.html");
-  let products = [
-    { name: "BMW", model: 2000, fuel: "Diesel" },
-    { name: "Ford", model: 2005, fuel: "Benzin" },
-    { name: "Opel", model: 2010, fuel: "Gas" },
-    { name: "Audi", model: 2020, fuel: "Diesel" },
+  //console.log(req.query)
+  // res.sendFile(__dirname+'/index.html')
+  let products = [{
+      name: "BMW",
+      model: 2000,
+      fuel: "Diesel"
+    },
+    {
+      name: "Ford",
+      model: 2005,
+      fuel: "Benzin"
+    },
+    {
+      name: "Opel",
+      model: 2010,
+      fuel: "Gas"
+    },
+    {
+      name: "Audi",
+      model: 2020,
+      fuel: "Diesel"
+    },
   ];
   res.render("index", {
     title: "Home",
     content: "This is home page",
-    dark: true,
+    dark: req.query.dark==="true" ? true : false,
     data: products
   });
 });
 
 app.get("/about", (req, res) => {
-  //res.sendFile(__dirname + "/about.html");
-  res.render("about", { title: "About", content: "This is about page" });
+  // res.sendFile(__dirname+'/about.html')
+  res.render("about", {
+    title: "About",
+    content: "This is About page",
+    dark: req.query.dark==="true" ? true : false,
+  });
 });
 
 app.get("/services", (req, res) => {
-  //res.sendFile(__dirname + "/services.html");
+  // res.sendFile(__dirname+'/services.html')
   res.render("services", {
     title: "Services",
-    content: "This is services page",
+    content: "This is Services page",
+    dark: req.query.dark==="true" ? true : false,
   });
-  //console.log(__dirname)
+});
+
+app.get("/products", (req, res) => {
+  // res.sendFile(__dirname+'/services.html')
+  res.render("products", {
+    title: "Products",
+    content: "This is Products page",
+    dark: req.query.dark==="true" ? true : false,
+    data: data
+  });
 });
 
 app.get("/api", (req, res) => {
-  res.json([
-    {
-      name: "Fatih",
-      age: 30,
-    },
-  ]);
+  res.json([{
+    name: "Mostafa",
+    age: 30,
+  }, ]);
 });
 
 app.listen(app.get("port"), () => {
