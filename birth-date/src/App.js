80fecb944e535495bf2887b3c4c0e149c9c92301
fatihyanik <fatihyanik07@gmail.com>
2022-02-14 19:56:12 +0100

    I finished birth-date

--- birth-date/src/App.js
+++ birth-date/src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
-
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
 function App() {
+  const [people, setPeople] = useState(data);
   return (
-    <div className="App">
-
-    </div>
+    <main>
+      <section className="container">
+        <h3>{people.length} birthday today</h3>
+        <List people={people} />
+        <button onClick={() => setPeople([])}>clear all</button>
+      </section>
+    </main>
   );
 }
 
