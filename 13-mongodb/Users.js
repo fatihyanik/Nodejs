a13e84ef4e60caecc7aa9fe008664fe1d40dd0c3
fatihyanik <fatihyanik07@gmail.com>
2022-02-15 16:21:28 +0100

    I added Users.js

--- 13-mongodb/Users.js
+++ 13-mongodb/Users.js
@@ -1,16 +1,19 @@
 const mongoose = require('mongoose');
 const {Schema} = require('mongoose')
-// define schema for users collection
+
+
+// define Schema
 const usersSchema = new Schema({
     first_name: String,
     last_name: String,
     email: String,
     password: String,
     phone: Number,
-    age: Number
-}, {collection: "users"})
+    age: Number 
+}, {collection:"users"})
 
-// define a model
+// define a model:
 const Users = mongoose.model('Users', usersSchema)
+
 
 module.exports = Users
--- 13-mongodb/app.js
+++ 13-mongodb/app.js
@@ -1,44 +1,70 @@
-const mongoose = require('mongoose');
-const Users = require('./Users');
-mongoose.connect('mongodb://localhost/bookstore', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}/* , ()=>{
-    console.log("MongoDb Connected ...");
-} */)
+const mongoose = require("mongoose");
+const Users = require("./Users");
+mongoose.connect("mongodb://localhost/bookstore", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  autoIndex: true,
+});
+// mongoose.createConnection('mongodb://localhost/bookstore',{
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// })
 
-mongoose.connection.on('open', ()=>{
+mongoose.connection
+  .on("open", () => {
     console.log("MongoDb Connected ...");
-}).on('error',error=>{
+  })
+  .on("error", (error) => {
     console.log(error);
-})
+  });
 
-// get all data from users
-Users.find().then(u=>{
-    //console.log(u);
-}).catch(error=>{
+Users.find()
+  .then((u) => {
+    //console.log(u)
+  })
+  .catch((error) => {
     console.log(error);
-})
+  });
 
 // insert a new user, if email not exist
 // check if email exist
-Users.find({email: "rrr@rrr.rr"}).then(u=>{
-    if(u.length>0){ // email exist
-        console.log(`The Email is already exist`);
-    }else{ // email not exist
-        Users.create({
-            first_name: "NODEJS",
-            last_name: "EXPRESS",
-            email: "rrr@rrr.rr",
-            password: 123,
-            phone: 123,
-            age: 12
-        }).then(p=>{
-            console.log(p);
-        }).catch(error=>{
-            console.log(error);
-        })
-    }
-}).catch(error=>{
-    console.log(error);
+// Users.find({email: "rrr@rrr.rrr"}).then(u=>{
+//     if(u.length > 0){// email exist
+//         console.log(`the Email is already exist`)
+//     }else{// email not exist
+
+//     }
+// }).catch(error=>{
+//     console.log(error)
+// })
+
+Users.create({
+  first_name: "NODEJS",
+  last_name: "EXPRESS",
+  email: "rrr@rrr.rrr",
+  password: 123,
+  phone: "123456",
+  age: 32,
 })
+  .then((p) => {
+    console.log(p);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+// try to edit one user first_name, age
+// Users.findOneAndUpdate({email: "mostafa.othman@digitalcareerinstitute.org"}, {$set: {first_name: "Edited", age:70}}, {new: true}).then(u=>{
+//     console.log(u)
+// }).catch(error=>{
+//     console.log(error)
+// })
+Users.findOneAndUpdate(
+  { email: "fatih@gmail.com" },
+  { $set: { first_name: "Edited", age: 50 } },
+  { new: true },
+  (error, result) => {
+    if (error) throw error;
+    console.log(result);
+  }
+);
