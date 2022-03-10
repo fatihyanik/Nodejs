6e1ed1693591b36d6bbb34204b324331139738b2
fatihyanik <fatihyanik07@gmail.com>
2022-03-10 13:19:11 +0100

    I added connection mysql

--- 21-express-mysql/models/db.js
+++ 21-express-mysql/models/db.js
@@ -1,7 +1,8 @@
 const mysql = require('mysql');
 require('dotenv').config();
 
-function getUsers(){
+
+function getUsers() {
     // start mysql connection
     const con = mysql.createConnection({
         host: process.env.HOST,
@@ -11,21 +12,57 @@ function getUsers(){
         database: process.env.DB_NAME
     });
     con.connect(error => {
-        if(error){
+        if (error) {
             console.log(error);
-        }else{
+        } else {
             // there are no errors with connections
             // we can run queries to database
-            con.query('SELECT * FROM users', (error, result, fields)=>{
-                if(error){
+            con.query('SELECT * FROM users', (error, result, fields) => {
+                if (error) {
                     console.log(error);
-                }else{
-                    console.log(result);
-                    console.log(fields);
+                } else {
+                    //console.log(result);
+                    //console.log(fields);
                 }
             })
         }
     })
 }
 
-getUsers()
+let con = null;
+
+function connect() {
+    return new Promise((resolve, reject) => {
+        if (con) {
+            // con is not null
+            if (con.state === 'disconnected') {
+                //try to connect
+                con.connect(error => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        resolve()
+                    }
+                })
+            } else {
+                resolve()
+            }
+        } else {
+            // con is null
+            con = mysql.createConnection({
+                host: process.env.HOST,
+                port: process.env.DB_PORT,
+                user: process.env.DB_USER,
+                password: process.env.DB_PASSWORD,
+                database: process.env.DB_NAME
+            })
+            con.connect(error => {
+                if (error) {
+                    reject(error)
+                } else {
+                    resolve()
+                }
+            })
+        }
+    })
+}
